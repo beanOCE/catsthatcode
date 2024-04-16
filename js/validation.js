ae5cdf37ec9bacea44fba9f6932ff8c79740e85f
Maeve Kelly <89049979+mave1@users.noreply.github.com>
2024-04-17 01:43:05 +1000

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -1,72 +1,62 @@
-const form = document.forms('create-form');
-const title = document.forms('title');
-const tags = document.forms('tags');
-const content = document.forms('content');
 
 
-if (form) {
-    form.addEventListener('submit', validateForm);
-} else {
-    console.error('Unable to find the registration form; no validation will be performed');
-}
 
-if (title) {
-    form.addEventListener('submit', validateForm);
-} else {
-    console.error('Unable to find the registration form; no validation will be performed');
-}
+window.onload = function () {
+    const createForm = document.getElementById('create-form');
+    const createBtn = document.getElementById('create');
+    const resetBtn = document.getElementById('reset');
 
-if (tags) {
-    form.addEventListener('submit', validateForm);
-} else {
-    console.error('Unable to find the registration form; no validation will be performed');
-}
+    if (createForm) {
+        createBtn.addEventListener('click', function(event) {
+            alert("1TEST");
+            event.preventDefault();
+            const title = document.getElementById('title');
+            const tags = document.getElementById('tags');
+            const content = document.getElementById('content');
+            
+            if (title.value == '') {
+                event.preventDefault();
+                document.getElementById("title_error").innerHTML = "This is a required field";
+            } else {
+                document.getElementById("title_error").innerHTML = "";
+            }
+                
+            if (tags.value == '') {
+                event.preventDefault();
+                document.getElementById("tags_error").innerHTML = "This is a required field";
+            } else {
+                document.getElementById("tags_error").innerHTML = "";
+            }
+                
+            if (content.value == '') {
+                alert("v");
+                event.preventDefault();
+                document.getElementById("content_error").innerHTML = "This is a required field";
+            } else if (content.value.length > 80){ 
+                alert("TEST");
+                event.preventDefault();
+                document.getElementById("content_error").innerHTML = "Content must be less than 80 characters";
+            } else {
+                document.getElementById("content_error").innerHTML = "";
+            }
 
 
-if (content) {
-    alert("test");
-    form.addEventListener('submit', validateForm);
-} else {
-    alert("test fail");
-    console.error('Unable to find the registration form; no validation will be performed');
-}
+            if ((title.value != '' && tags.value != '' && content.value != '')){
+                //create
+            }
+        });
 
+        resetBtn.addEventListener('click', function(event) {
+            event.preventDefault();
+            document.getElementById("title").value = "";
+            document.getElementById("tags").value = "";
+            document.getElementById("content").value = "";
+            document.getElementById("title_error").innerHTML = "";
+            document.getElementById("tags_error").innerHTML = "";
+            document.getElementById("content_error").innerHTML = "";
+        });
 
-function validateForm(event) {
-
-    const errorMsg = "This field is required";
-
-    if (form.Title.value == ' ') {
-        event.preventDefault();
-        document.getElementById("title_error").innerHTML = "required field";
-    }
-
-    if (form.Tags / Keywords.value == ' ') {
-        event.preventDefault();
-        document.getElementById("tags_error").innerHTML = "required field";
-    }
-
-    if (form.Content.value == ' ') {
-        event.preventDefault();
-        document.getElementById("content_error").innerHTML = "required field";
-    }
-}
-
-function create() {
-    var contentTXT = form.Content.value;
-
-    console.log("test");
-
-    if (!(contentTXT.length > 80)) {
-        //create post
     } else {
-        document.getElementById("contentError").innerHTML = "Content needs to be less than 80 characters";
+        console.error('Unable to find the registration form; no validation will be performed');
     }
-
-}
-
-function reset() {
-    document.getElementById("title").innerHTML = "";
-    document.getElementById("tags").innerHTML = "";
-    document.getElementById("content").innerHTML = "";
-}
+};
