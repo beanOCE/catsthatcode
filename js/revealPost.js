495ac049cb8ad6e049ece18c5196eefb1e148b40
bean3 <benwwaghorn@gmail.com>
2024-04-18 23:48:08 +1000

    Show/Hide added to archive.html

--- js/revealPost.js
+++ js/revealPost.js
@@ -1,13 +1,51 @@
+function showPost1() {
+  const revealBtn = document.getElementById("reveal1");
+  const revealPost = document.getElementById("archive1");
 
-function showPost() {
-    const revealBtn = document.getElementById("reveal"); 
-    const revealPost = document.getElementById("archive"); 
-
-    if (revealPost.style.display === "none") {
-        revealPost.style.display = "block";
-        revealBtn.innerHTML = "Hide Post";
-      } else {
-        revealPost.style.display = "none";
-        revealBtn.innerHTML = "Show Post";
-      }
+  if (revealPost.style.display === "none") {
+    revealPost.style.display = "block";
+    revealBtn.innerHTML = "Hide Post";
+  } else {
+    revealPost.style.display = "none";
+    revealBtn.innerHTML = "Show Post";
   }
+}
+
+function showPost2() {
+  const revealBtn = document.getElementById("reveal2");
+  const revealPost = document.getElementById("archive2");
+
+  if (revealPost.style.display === "none") {
+    revealPost.style.display = "block";
+    revealBtn.innerHTML = "Hide Post";
+  } else {
+    revealPost.style.display = "none";
+    revealBtn.innerHTML = "Show Post";
+  }
+}
+
+function showPost3() {
+  const revealBtn = document.getElementById("reveal3");
+  const revealPost = document.getElementById("archive3");
+
+  if (revealPost.style.display === "none") {
+    revealPost.style.display = "block";
+    revealBtn.innerHTML = "Hide Post";
+  } else {
+    revealPost.style.display = "none";
+    revealBtn.innerHTML = "Show Post";
+  }
+}
+
+function showPost4() {
+  const revealBtn = document.getElementById("reveal4");
+  const revealPost = document.getElementById("archive4");
+
+  if (revealPost.style.display === "none") {
+    revealPost.style.display = "block";
+    revealBtn.innerHTML = "Hide Post";
+  } else {
+    revealPost.style.display = "none";
+    revealBtn.innerHTML = "Show Post";
+  }
+}
